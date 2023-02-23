b9c6ea686e06cc57d6ce087e52910074af45ac05
sjurlne <sjur.lone@gmail.com>
2023-02-23 13:38:36 +0100

    'Initial commit.'

---
+++ src/final_project_productivity/__init__.py
@@ -0,0 +1,1 @@
+"""Code for producing the results of the Final Project: Productivity Analysis Across Sectors project."""
---
+++ src/final_project_productivity/final/__init__.py
@@ -0,0 +1,5 @@
+"""Functions for formatting results."""
+
+from final_project_productivity.final.plot import plot_regression_by_age
+
+__all__ = [plot_regression_by_age]
