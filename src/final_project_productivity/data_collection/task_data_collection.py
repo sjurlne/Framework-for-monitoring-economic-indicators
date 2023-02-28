74c86936a7710288fde56f456e5d8cdaae9505c9
sjurlne <sjur.lone@gmail.com>
2023-02-28 12:23:31 +0100

    task implemented

--- src/final_project_productivity/data_collection/task_data_collection.py
+++ src/final_project_productivity/data_collection/task_data_collection.py
@@ -1,10 +1,44 @@
 """task for webscrapper"""
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 import pytask
-from web_scraper.py import
+from final_project_productivity.data_collection.web_scraper import execute_norway
+
+SITES = ["https://www.ssb.no/en/statbank/table/09170/", #Value Added
+         "https://www.ssb.no/en/statbank/table/09174/", #Hours
+         "https://www.ssb.no/en/statbank/table/09181/" #Capital
+         ]
+
+element_ids = ["ctl00_ContentPlaceHolderMain_VariableSelector1_VariableSelector1_VariableSelectorValueSelectRepeater_ctl01_VariableValueSelect_VariableValueSelect_ValuesListBox", #Variable Names
+               "ctl00_ContentPlaceHolderMain_VariableSelector1_VariableSelector1_VariableSelectorValueSelectRepeater_ctl02_VariableValueSelect_VariableValueSelect_SelectAllButton", #YEARS
+               "ctl00_ContentPlaceHolderMain_VariableSelector1_VariableSelector1_VariableSelectorValueSelectRepeater_ctl03_VariableValueSelect_VariableValueSelect_GroupingDropDown", #dropdown industry
+               "ctl00_ContentPlaceHolderMain_VariableSelector1_VariableSelector1_VariableSelectorValueSelectRepeater_ctl03_VariableValueSelect_VariableValueSelect_SelectAllButton",
+               "ctl00_ContentPlaceHolderMain_VariableSelector1_VariableSelector1_ButtonViewTable", #View Tables
+               "ctl00_ctl00_ContentPlaceHolderMain_ImageButtonpivotCCW", #Pivot Table
+               "ctl00_ctl00_ContentPlaceHolderMain_ShortcutFileFileTypeExcelX"] #download
+
+variable_names = [["Value added at basic prices. Constant 2015 prices (NOK million)", "Compensation of employees. Current prices (NOK million)"], 
+                  ["Total hours worked for employees and self-employed (million workhours)"],
+                  ["Fixed assets. Constant 2015 prices (NOK million)", "Gross fixed capital formation. Current prices (NOK million)", "Consumption of fixed capital. Current prices (NOK million)"]
+                  ]
+
+default_select = [["Output at basic values. Current prices (NOK million)"],
+                  ["Wages and salaries (NOK million)"],
+                  []
+                  ]
+
+new_names = ["value_added_norway.xlsx", #Value Added
+             "hours_norway.xlsx", #Capital Stocks
+             "capital_norway.xlsx"
+             ]
 
 def task_collect_data():
+    """
+    Initializes the web crawler and collect the relevant data, 
+    and puts it in the data folder.
+    """
+    
     PATH = "C:\Program Files (x86)\chromedriver.exe"
-
     download_dir = r"src\final_project_productivity\data\norway"
 
     options = Options()
@@ -18,3 +52,9 @@ def task_collect_data():
     options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome(PATH, options=options)
+
+    execute_norway(driver)
+
+
+
+
