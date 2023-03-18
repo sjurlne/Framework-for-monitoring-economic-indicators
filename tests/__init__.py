31eee674c167012906e74f86a2b41f93202a8107
sjurlne <sjur.lone@gmail.com>
2023-03-18 10:15:41 +0100

    some more tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,4 @@
 """This module is necessary to configure mypy's behavior for the tests."""
-#from tests import test_check_internet_connection_for_selenium
-#from tests import test_estimate
-#from tests import test_clean_data
+#from tests import test_estimate, test_clean_data, test_internet_check
 
-#__all__ = [test_check_internet_connection_for_selenium, test_estimate, test_clean_data]
+#__all__ = [test_internet_check, test_estimate, test_clean_data]
