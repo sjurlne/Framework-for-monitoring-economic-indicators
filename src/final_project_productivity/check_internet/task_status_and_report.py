5f07438f1aba9d688da3696d08ca00ec1017e123
sjurlne <sjur.lone@gmail.com>
2023-03-29 11:04:39 +0200

    Last version

--- src/final_project_productivity/check_internet/task_status_and_report.py
+++ src/final_project_productivity/check_internet/task_status_and_report.py
@@ -1,5 +1,6 @@
 from final_project_productivity.check_internet.check_internet import check_internet_connection_for_selenium, update_check
 from final_project_productivity.config import BLD
+import pandas as pd
 import csv
 import pytask
 
@@ -18,7 +19,7 @@ def task_last_check(produces):
 
 @pytask.mark.depends_on(BLD / "python" / "reports" / "last_updated.csv")
 @pytask.mark.produces(BLD / "python" / "reports" / "internet_check.csv")
-def task_internet_check(produces):
+def task_internet_check(produces, depends_on):
     """
     Checks the internet connection using the `check_internet_connection_for_selenium` function and writes a report
     to a CSV file. The report is used as a condition for running the web scraper.
@@ -26,11 +27,13 @@ def task_internet_check(produces):
     Args:
         produces (str): Path to the CSV file where the report will be saved.
     """
+    last_update = pd.read_csv(depends_on, header=None, names=None)
+    need_update = last_update.iloc[1, 0] == "True"
 
-
-    report = check_internet_connection_for_selenium(req_down_speed=down_speed, req_up_speed=up_speed)
-    #if report[0][1] == False:
-    #    raise LookupError("Internet check did not pass, and project can not complete.")
-    with open(produces, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(report)
+    if need_update:
+        report = check_internet_connection_for_selenium(req_down_speed=down_speed, req_up_speed=up_speed)
+        #if report[0][1] == False:
+        #    raise LookupError("Internet check did not pass, and project can not complete.")
+        with open(produces, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(report)
